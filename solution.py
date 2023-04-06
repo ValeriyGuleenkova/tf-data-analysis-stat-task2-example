d86ca8bed1951383ca7872e555488939d532af9a
ValeriyGuleenkova <64977472+ValeriyGuleenkova@users.noreply.github.com>
2023-04-06 12:37:02 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,19 +1,20 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import expon
+from scipy.stats import gamma
 
 chat_id = 215606022
 
 
 def solution(p: float, x: np.array) -> tuple:
-    TIME = 41
     alpha = 1 - p
-    stat = x.sum()
+    n = len(x)
     
-    left_edge_expon = expon.ppf(alpha / 2)
-    right_edge_expon = expon.ppf(1 - alpha / 2)
+    TIME = 41
     
-    left_edge_acceleration = (left_edge_expon + stat - 1/2) / (TIME ** 2)
-    right_edge_acceleration = (right_edge_expon + stat - 1/2) / (TIME ** 2)
+    left_edge_gamma = gamma.ppf(q = alpha / 2, a = n, scale = 1 / n)
+    right_edge_gamma = gamma.ppf(q = 1 - alpha / 2, a = n, scale = 1 / n)
+    
+    left_edge_acceleration = (left_edge_gamma + x.mean() - 1/2) / (TIME ** 2)
+    right_edge_acceleration = (right_edge_gamma + x.mean() - 1/2) / (TIME ** 2)
     
     return (left_edge_acceleration, right_edge_acceleration)
