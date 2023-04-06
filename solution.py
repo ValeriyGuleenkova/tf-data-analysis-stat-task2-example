6b7e0e7acecff39076f43011bc147fdf1db92dd3
ValeriyGuleenkova <64977472+ValeriyGuleenkova@users.noreply.github.com>
2023-04-06 15:20:21 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -14,7 +14,7 @@ def solution(p: float, x: np.array) -> tuple:
     left_edge_gamma = gamma.ppf(q = alpha / 2, a = n, scale = 1 / n)
     right_edge_gamma = gamma.ppf(q = 1 - alpha / 2, a = n, scale = 1 / n)
     
-    left_edge_acceleration = (left_edge_gamma + x.mean() - 1/2) / (TIME ** 2)
-    right_edge_acceleration = (right_edge_gamma + x.mean() - 1/2) / (TIME ** 2)
+    left_edge_acceleration = 2*(left_edge_gamma + x.mean() - 1/2) / (TIME ** 2)
+    right_edge_acceleration = 2*(right_edge_gamma + x.mean() - 1/2) / (TIME ** 2)
     
     return (left_edge_acceleration, right_edge_acceleration)
